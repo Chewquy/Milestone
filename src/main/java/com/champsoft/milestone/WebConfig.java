ffb0a2f1edf83828965b7a2626ffb10ec3fe3e54
Thomas Angers <thomas.angers@icloud.com>
2025-12-01 09:52:51 -0500

    Change in the config

--- src/main/java/com/champsoft/milestone/WebConfig.java
+++ src/main/java/com/champsoft/milestone/WebConfig.java
@@ -12,16 +12,14 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
-// your React dev URL(s)
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:3000",
+                                "https://jazzy-babka-92a27a.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
-                registry.addMapping("/**").allowedOrigins("https://jazzy-babka-92a27a.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-
             }
         };
     }
